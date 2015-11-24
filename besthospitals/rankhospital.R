5ce69f480eb901c52b9f9703a007c79779a5f73e
Marcelo Tournier <dr.tournier@gmail.com>
2015-11-23 22:55:21 -0200

    rankhospitals ready

--- besthospitals/rankhospital.R
+++ besthospitals/rankhospital.R
@@ -1,38 +1,36 @@
-best <- function(x,y) {
-  # best <- function(x = state, y = outcome) {
-  ## Read outcome data
+rankhospital <- function(x,y,num="best") {
   outcome <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
   lista = c('heart attack','heart failure','pneumonia')
-  ## Check that state and outcome are valid
+## Check that state and outcome are valid
   if(x %in% outcome[,7] == FALSE){
     stop('invalid state')
-  }
+    }
   if(y %in% lista == FALSE){
     stop('invalid outcome')
-  }
+    }
   if(y == 'heart attack'){
-        r=11
-        # heart attack = [,11]
-  }
+    r=11
+    # heart attack = [,11]
+    }
   if(y == 'heart failure'){
     r=17
     # heart failure = [,17]
-  }
+    }
   if(y == 'pneumonia'){
     r=23
     # pneumonia = [,23]
-  }
-  ## Return hospital name in that state with lowest 30-day death
-  ## rate
+    }
   rank=outcome[outcome$State == x,]
   rank[,r]=gsub("Not Available",NA, rank[,r])
   rank[,r]=as.numeric(as.character(rank[,r]))
-  p=which.min(rank[,r])
-  tie=rank[p,2]
-  if(length(tie)>1){
-    tie2=sort(tie, decreasing = FALSE)
-    tie2[1]
-    }else{
-      tie
-    }
-}
+  ord=rank[ order(rank[,r], rank[,2]), ]
+  if(num == "best"){
+    head(ord[,2],1)
+    } else if(num == "worst"){
+    tail(ord[,2],1)
+    } else if(is.numeric(num)) {
+    ord[num,2]
+  }
+  }
+## Return hospital name in that state with the given rank
+## 30-day death rate
